efc032c3ce830b8595edc32aa549cf30556d2f6d
amirafatnassi <amirafatnassi88@gmail.com>
2022-08-29 22:33:39 +0100

    json db server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('tempButton') buttontemp: any;
   title = 'angular-employees-app';
+
+  employeeForm: FormGroup;
+
+  educationOptions = [
+    '10th pass',
+    'diploma',
+    'graduate',
+    'post graduate',
+    'PhD',
+  ];
+
+  constructor(private fb: FormBuilder) {
+    this.employeeForm = fb.group({});
+  }
+
+  ngOnInit(): void {
+    this.employeeForm = this.fb.group({
+      firstname: this.fb.control(''),
+      lastname: this.fb.control(''),
+      birthday: this.fb.control(''),
+      gender: this.fb.control(''),
+      education: this.fb.control(''),
+      company: this.fb.control(''),
+      jobExperience: this.fb.control(''),
+      salary: this.fb.control(''),
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.buttontemp.nativeElement.click();
+  }
 }
